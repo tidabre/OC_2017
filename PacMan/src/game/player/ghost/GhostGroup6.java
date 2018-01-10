b3c5184f8a4687d60df6a7d47affb3a600bf5b2f
Timon Bressgott <timon@bressgott.de>
2018-01-10 12:18:03 +0100

    First idea of neural net solution

--- PacMan/src/game/player/ghost/GhostGroup6.java
+++ PacMan/src/game/player/ghost/GhostGroup6.java
@@ -1,5 +1,20 @@
 package game.player.ghost;
 
-public class GhostGroup6 {
+import game.core.Game;
+import gui.AbstractGhost;
+
+public class GhostGroup6 extends AbstractGhost {
+
+	@Override
+	public int[] getActions(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGhostGroupName() {
+		// TODO Auto-generated method stub
+		return "GhostGroup6";
+	}
 
 }
